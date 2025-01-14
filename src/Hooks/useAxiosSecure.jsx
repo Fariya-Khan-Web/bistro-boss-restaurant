f5e63f53042598b38ee78c6fdbfae8805cb0f566
Fariya <fariya.webdev@gmail.com>
2025-01-14 13:25:59 +0600

    feat: logout in varify error

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 
 const axiosSecure = axios.create({
@@ -7,26 +10,33 @@ const axiosSecure = axios.create({
 
 const useAxiosSecure = () => {
 
+    const {logOut} = useContext(AuthContext)
+    const navigate = useNavigate()
+
     axiosSecure.interceptors.request.use(function (config) {
         const token = localStorage.getItem('access-token')
         config.headers.authorization = `Bearer ${token}`
         return config;
     },
-     function (error) {
-        // do something with request error
-        return Promise.reject(error)
-    })
+        function (error) {
+            // do something with request error
+            return Promise.reject(error)
+        })
 
 
     // intercept 401 & 403
-    axiosSecure.interceptors.response.use(function (response){
+    axiosSecure.interceptors.response.use(function (response) {
         return response;
-    }, 
-     function (error) {
-         const status = error.response.status;
-         console.log('in the intercepter' , error, status)
-        return Promise.reject(error)
-    }) 
+    },
+        function (error) {
+            const status = error.response.status;
+            console.log('in the intercepter', error, status)
+            if (status === 401 || status === 403) {
+                logOut()
+                navigate('/auth')
+            }
+            return Promise.reject(error)
+        })
 
 
     return axiosSecure
